ec571d97e75ba1d7aebe862d75be700d3787eb7a
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-19 05:42:38 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,5 @@
-require 'pry'
-def consolidate_cart(cart:[])
-  cart.each_with_object({}) do |value, cart_array|
-    value.each do |type, attrs|
-      if cart_array[type]
-        attrs[:count] += 1
-      else
-        attributes[:count] = 1
-        cart_array[type] = attrs
-      end
-    end
-  end
+def consolidate_cart(cart)
+  # code here
 end
 
 def apply_coupons(cart, coupons)
